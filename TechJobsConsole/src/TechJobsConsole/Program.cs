e3a15b8464dad89f19546c326aa60bbdbcd69881
AncientRustyRobot <SportsDeath@aol.com>
2020-01-06 13:34:58 -0600

    final

---
+++ TechJobsConsole/src/TechJobsConsole/Program.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TechJobsConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create two Dictionary vars to hold info for menu and data
+
+            
+            Dictionary<string, string> actionChoices = new Dictionary<string, string>();
+            actionChoices.Add("search", "Search");
+            actionChoices.Add("list", "List");
+
+            
+            Dictionary<string, string> columnChoices = new Dictionary<string, string>();
+            columnChoices.Add("core competency", "Skill");
+            columnChoices.Add("employer", "Employer");
+            columnChoices.Add("location", "Location");
+            columnChoices.Add("position type", "Position Type");
+            columnChoices.Add("all", "All");
+
+            Console.WriteLine("Welcome to LaunchCode's TechJobs App!");
+
+            
+            while (true)
+            {
+
+                string actionChoice = GetUserSelection("View Jobs", actionChoices);
+
+                if (actionChoice.Equals("list"))
+                {
+                    string columnChoice = GetUserSelection("List", columnChoices);
+
+                    if (columnChoice.Equals("all"))
+                    {
+                        PrintJobs(JobData.FindAll());
+                    }
+                    else
+                    {
+                        List<string> results = JobData.FindAll(columnChoice);
+
+                        Console.WriteLine("\n* All " + columnChoices[columnChoice] + " Values *");
+                        foreach (string item in results)
+                        {
+                            Console.WriteLine(item);
+                        }
+                    }
+                }
+                else // choice is "search"
+                {
+                    // How does the user want to search (by skill or employer)
+                    string columnChoice = GetUserSelection("Search", columnChoices);
+
+                    // What is their search term?
+                    Console.WriteLine("\nSearch term: ");
+                    string searchTerm = Console.ReadLine();
+
+                    List<Dictionary<string, string>> searchResults;
+
+                    // Get results
+                    if (columnChoice.Equals("all"))
+                    {
+                        searchResults = JobData.FindByValue(searchTerm);
+                        PrintJobs(searchResults);
+                        //Console.WriteLine("Search all fields not yet implemented.");
+                    }
+                    else
+                    {
+                        searchResults = JobData.FindByColumnAndValue(columnChoice, searchTerm);
+                        PrintJobs(searchResults);
+                    }
+                }
+            }
+        }
+
+        
+        private static string GetUserSelection(string choiceHeader, Dictionary<string, string> choices)
+        {
+            int choiceIdx;
+            bool isValidChoice = false;
+            string[] choiceKeys = new string[choices.Count];
+
+            int i = 0;
+            foreach (KeyValuePair<string, string> choice in choices)
+            {
+                choiceKeys[i] = choice.Key;
+                i++;
+            }
+
+
+            do
+            {
+                Console.WriteLine("\n" + choiceHeader + " by:");
+
+                for (int j = 0; j < choiceKeys.Length; j++)
+                {
+                    Console.WriteLine(j + " - " + choices[choiceKeys[j]]);
+                }
+
+                string input = Console.ReadLine();
+                choiceIdx = int.Parse(input);
+
+                if (choiceIdx < 0 || choiceIdx >= choiceKeys.Length)
+                {
+                    Console.WriteLine("Invalid choices. Try again.");
+                }
+                else
+                {
+                    isValidChoice = true;
+                }
+
+            } while (!isValidChoice);
+
+            return choiceKeys[choiceIdx];
+        }
+
+        private static void PrintJobs(List<Dictionary<string, string>> someJobs)
+        {
+            //Console.WriteLine("no results to show");
+            int count = someJobs.Count;
+            if (count == 0)
+            {
+                Console.WriteLine("No result");
+            }
+            else
+            {
+                foreach (Dictionary<string, string> item in someJobs)
+                {
+                    Console.WriteLine("\n*");
+                    foreach (KeyValuePair<string, string> itemline in item)
+                    {
+                        if (itemline.Key != "S.no")
+                            Console.WriteLine(itemline.Key + " : " + itemline.Value);
+                    }
+                    Console.WriteLine("*\n\n");
+                }
+            }
+        }
+    }
+}
